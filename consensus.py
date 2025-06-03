4070a47102ef3278723a3080850725a8fee183d7
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2025-06-03 11:58:30 +0100

    floodset

--- consensus.py
+++ consensus.py
@@ -1,8 +1,14 @@
 from graphs import Vertice, Graph, Edge
 from typing import TypeVar, Generic
 from enum import Enum
+import random
 
 T = TypeVar("T")
+
+
+class ProcessStatus(Enum):
+    CORRECT = 0
+    DEAD = 1
 
 
 class Result(Enum):
@@ -19,6 +25,14 @@ class Process(Vertice, Generic[T]):
         self.w = [initial_value]
         self.rounds = 0
         self.decision = None
+        self.status = ProcessStatus.CORRECT
+
+    def send_w(self, process: "Process"):
+        process.receive(self.w)
+
+    def receive(self, w: list[T]):
+        if self.status != ProcessStatus.DEAD:
+            self.w = set(self.w + w)
 
     def decision_rule(self, prescribed_value: T):
         if len(self.w) == 1:
@@ -43,9 +57,35 @@ def generate_network(network_size: int) -> Graph[Process[Result]]:
 
 def floodset(faulty_processes: int, network: Graph[Process[Result]]):
 
+    failed_processes = 0
+    
+    for round in range(0, faulty_processes + 1):
 
-    pass
+        # Have every process send each other its values
+        for process_i in range(0, len(network.vertices)):
+
+            process_i_actual = network.vertices[process_i]
+            if process_i_actual.status == ProcessStatus.DEAD:
+                continue
+
+            for process_j in range(process_i + 1, len(network.vertices)):
+
+                # Whilst sending, make it possible for process i to crash
+                if failed_processes < faulty_processes:
+                    if random.randint(0, 20) == 0:
+                        failed_processes += 1
+                        process_i_actual.status = ProcessStatus.DEAD
+                        break
+                
+                # Send w to process j
+                process_i_actual.send_w(network.vertices[process_j])
+
+    # Now, apply the decision rule
+    # I can't be asked to do this right now
+    # But basically check each non-faulty process' W set
 
 
-def run_floodset(faulty_processes: int, network_size: int):
+def run_floodset(faulty_processes: int, network_size: int, prescribed_value: T):
 
+    graph = generate_network(network_size)
+    floodset(faulty_processes, graph)
